443e9c4dcdb097334930bd39a721b8e3021225e6
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-01-06 17:47:49 +0000

    Reviews Using React JS

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,68 @@
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {
+    index: 0,
+  }
+
+  leftArrowClicking = () => {
+    this.setState(prevState => ({index: prevState.index - 1}))
+  }
+
+  rightArrowClicking = () => {
+    this.setState(prevState => ({index: prevState.index + 1}))
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {index} = this.state
+    const personProfile = reviewsList[index]
+    console.log(personProfile)
+    return (
+      <div className="home">
+        <h1 className="heading">Reviews</h1>
+        <div className="arrow-container">
+          <div className="left-arrow-container">
+            <button
+              className="btn"
+              type="button"
+              onClick={this.leftArrowClicking}
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+                alt="left arrow"
+                className="image-left"
+              />
+            </button>
+          </div>
+          <div className="card-container">
+            <img
+              src={personProfile.imgUrl}
+              alt={personProfile.username}
+              className="image"
+            />
+            <h1 className="head">{personProfile.username}</h1>
+            <p className="parag-company">{personProfile.companyName}</p>
+            <p className="parag-para">{personProfile.description}</p>
+          </div>
+          <div className="right-arrow-container">
+            <button
+              type="button"
+              className="btn"
+              onClick={this.rightArrowClicking}
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+                alt="right arrow"
+                className="image-right"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
